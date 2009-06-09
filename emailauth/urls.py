6e91b86d0afcd49b330533176a36b46b9e7d21ff
Yuri Baburov <burchik@gmail.com>
2009-06-10 05:35:20 +0700

    Fixed mistake in one of the paths, account page is now /account/profile/

--- emailauth/urls.py
+++ emailauth/urls.py
@@ -2,10 +2,10 @@ from django.conf.urls.defaults import patterns
 from django.conf.urls.defaults import url
 
 urlpatterns = patterns('emailauth.views',
-    url(r'^account/$', 'account', name='emailauth_account'),
+    url(r'^account/profile/$', 'account', name='emailauth_account'),
     url(r'^register/$', 'register', name='register'),
     url(r'^register/continue/(?P<email>.+)/$', 'register_continue', name='emailauth_register_continue'),
-    url(r'^verify/(?P<verification_key>\w+)/$', 'emailauth.views.verify', name='emailauth_verify'),
+    url(r'^verify/(?P<verification_key>\w+)/$', 'verify', name='emailauth_verify'),
     url(r'^resetpassword/$', 'request_password_reset', name='emailauth_request_password_reset'),
     url(r'^resetpassword/continue/(?P<email>.+)/$', 'request_password_reset_continue', 
         name='emailauth_request_password_reset_continue'),
